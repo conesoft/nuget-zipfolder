3dd2a87a32409758ebbb2ec64ed0112d0753bc57
David Spörri <davepermen@hotmail.com>
2023-09-05 02:54:11 +0200

    Projektdateien hinzufügen.

---
+++ New Zip Api Tests (.NET 8)/PositionWrapperStream.cs
@@ -0,0 +1,29 @@
+﻿class PositionWrapperStream : Stream
+{
+    private long pos = 0;
+
+    public override bool CanSeek { get { return false; } }
+    public override bool CanWrite { get { return true; } }
+
+    public override long Position
+    {
+        get { return pos; }
+        set { throw new NotSupportedException(); }
+    }
+
+    public override void Write(byte[] buffer, int offset, int count)
+    {
+        pos += count;
+    }
+
+    public override void Flush()
+    {
+    }
+
+    /* implementation garbage */
+    public override long Seek(long offset, SeekOrigin origin) => throw new NotImplementedException();
+    public override void SetLength(long value) => throw new NotImplementedException();
+    public override bool CanRead => throw new NotImplementedException();
+    public override int Read(byte[] buffer, int offset, int count) =>  throw new NotImplementedException();
+    public override long Length => throw new NotImplementedException();
+}
---
+++ New Zip Api Tests (.NET 8)/Program.cs
@@ -0,0 +1,46 @@
+﻿using Humanizer;
+using System.Diagnostics;
+
+var source = @"C:\Users\davep\Downloads\TNG\Season 1";
+var target = @"C:\Users\davep\Downloads\TNG\Season 1.zip";
+
+var timer = new Stopwatch();
+timer.Start();
+{
+    var c = 12595912021;
+    Console.WriteLine($"{c} - {c.Bytes()}");
+    var b = CountFast_(source);
+    Console.WriteLine($"{b} - {b.Bytes()}");
+
+    var x = Count_(source);
+    Console.WriteLine($"{x} - {x.Bytes()}");
+    //var y = WriteZip_(source, target);
+    //Console.WriteLine($"{y} - {y.Bytes()}");
+    var z = new FileInfo(target).Length;
+    Console.WriteLine($"{z} - {z.Bytes()}");
+}
+timer.Stop();
+Console.WriteLine(timer.Elapsed.Humanize());
+
+static long CountFast_(string source)
+{
+    return 0;
+}
+
+static long Count_(string source)
+{
+    using var stream = new PositionWrapperStream();
+    return WriteZipInternal_(source, stream);
+}
+
+static long WriteZip_(string source, string target)
+{
+    using var stream = new FileAppendOnlyWrapperStream(File.Open(target, FileMode.CreateNew));
+    return WriteZipInternal_(source, stream);
+}
+
+static long WriteZipInternal_(string source, Stream stream)
+{
+    System.IO.Compression.ZipFile.CreateFromDirectory(source, stream, System.IO.Compression.CompressionLevel.NoCompression, false);
+    return stream.Position;
+}
