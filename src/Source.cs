815bc8ca4ae46585aed56573370df99059ef93db
David Spörri <davepermen@hotmail.com>
2024-02-09 23:46:23 +0100

    LET'S GO!!!

--- src/Source.cs
+++ src/Source.cs
@@ -5,5 +5,5 @@ public record Source(string From, string To, bool WithSubdirectories = true)
     public static implicit operator Source(string from) => new(from, string.Empty);
     public static implicit operator Source((string from, string to, bool withSubdirectories) source) => new(source.from, source.to, source.withSubdirectories);
 
-    public static Source Directory(string From, string To, bool WithSubdirectories = true) => new(From, To, WithSubdirectories);
-};
+    public static Source Directory(string from, string to, bool withSubdirectories = true) => new(from, to, withSubdirectories);
+}
